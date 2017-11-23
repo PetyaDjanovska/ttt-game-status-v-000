dccbfc147b09ffc125128ad49509f0232d4b0bb4
Petya Djanovska <petya.djanovska@gmail.com>
2017-11-23 16:06:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,37 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],  # Bottom row
+  [0,3,6],  # left column
+  [1,4,7],  # middle column
+  [2,5,8],  # right column
+  [0,4,8],  # left diagonal
+  [2,4,6]  # right diagonal
+]
+
+def won? (board)
+ WIN_COMBINATIONS.each do |win_combination|
+   win_index_1 = win_combination[0]
+   win_index_2 = win_combination[1]
+   win_index_3 = win_combination[2]
+
+   position_1 = board[win_index_1]
+   position_2 = board[win_index_2]
+   position_3 = board[win_index_3]
+
+
+        #if position_taken?(board,win_index_1) &&
+        #      position_taken?(board, win_index_2) &&
+        #      position_taken?(board, win_index_3)
+        #        return false
+        if [position_1, position_2, position_3].all? {|symbol| symbol == "X"} ||
+           [position_1, position_2, position_3].all? {|symbol| symbol == "O"}
+          return win_combination
+        else
+          return false
+        end
+  end
+end
